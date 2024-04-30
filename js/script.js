c9e74219f27bc60f90f7146b6080e148f3ef4f6a
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-04-30 07:47:20 -0300

    Base dos exemplos de javascript.

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+//JavaScript básico tópicos
+
+//1.Declaração de variáveis
+
+//2.Hoisting
+ 
+//3.Tipos de dados
+ 
+//4.Operadores
+ 
+//5.Estruturas condicionais
+
+//6.Estruturas de repetição
+
+//7.Funções
+
+//8.Arrays
+
+//9.Objetos
+
+//10.Funções de array
+
+
