3736b9a62cd99fb0b3ec65b25e09d1a21d999e19
Murillo77 <murillofsantanna@gmail.com>
2024-04-30 09:50:52 -0300

    Arrays, forEach append.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 
 
 //JavaScript básico tópicos
-console.log("Olá Mundo!")
+// console.log("Olá Mundo!");
 
-//Capturar o botao pelo id e atrelar a ele um evento de click
-//const btnElemento = document.getElementById("botao");
-//btnElemento.addEventListener("click", function () {
-//    console.log("Botão testado!")
-//})
-
+// //Capturar o botão pelo ID e atrelar a ele um evento de click.
+// const btnElemento = document.getElementById("botao");
+// btnElemento.addEventListener("click", function(){
+//     console.log("Botão testado.");
+// });
 
 //1.Declaração de variáveis
 // var nome = "João";
@@ -19,25 +18,58 @@ console.log("Olá Mundo!")
 // console.log("Idade : " + idade);
 // idade = 21;
 
-
 //2.Hoisting
+//Diferença entre var e let
+// var nome = "João";
 
-//var nome = "Joao"
+// if(true){
+//     let nome = "Silva";
+// }
 
-//if(true){
-//    let nome = "Silva"
-//}
+// console.log(nome);
 
-//console.log(nome)
-
- 
 //3.Tipos de dados
- 
+    
+let tiposDeDados = [
+    "Number",
+    "String",
+    "Boolean",
+    "Null",
+    "Undefined",
+    "Object",
+    "Array",
+    "Function"
+]
+
+//Imprimindo o array com console.table.
+console.table(tiposDeDados);
+
+
 //4.Operadores
  
 //5.Estruturas condicionais
 
 //6.Estruturas de repetição
+// //Imprimindo Array com forEach com função anônima
+// tiposDeDados.forEach(function(tipo){
+//     console.log(tipo);
+// });
+// //Imprimindo Array com forEach com arrowFunction
+// tiposDeDados.forEach((tipo)=>{
+//     console.log(tipo);
+// });
+//Itera sobre um array de tipos de dados que serão inseridos em uma lista ul no html de id tipo-dados
+//Recuperando a lista ul através do id
+let listaTiposDeDados = document.getElementById("tipo-dados");
+
+tiposDeDados.forEach((tipo)=>{
+    //Criando um elemento <li> para inserir na <ul>
+    let itemLista = document.createElement("li");
+    //Adicionando o texto do tipo de dado
+    itemLista.innerText = tipo;
+     //Inserindo o elemento <li> na <ul>
+     listaTiposDeDados.appendChild(itemLista);
+});  
 
 //7.Funções
 
