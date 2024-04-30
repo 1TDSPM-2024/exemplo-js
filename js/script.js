f9ffea87a93222386021ba212d8c3ad03c2c8ced
arthuramosantos <arthuramossan@gmail.com>
2024-04-30 09:25:59 -0300

    aula

--- js/script.js
+++ js/script.js
@@ -8,9 +8,25 @@ btnElemento.addEventListener("click",function () {
     console.log("BOTÃO TESTADO. ");//deferJá utilizando o defer além do download e análise simultânea, o carregamento do seu HTML não será interrompido em nenhum momento para a execução dos scripts. Eles são executados apenas quando o carregamento de todo o documento estiver concluído.
 });//mostra o evento e parametro
 //1.Declaração de variáveis
+// var nome = "João";
+// let sobrenome = "Silva";
+//  console.log("Nome completo : " + nome +" "+ sobrenome);
+// //2.Declaração de constantes
+// const idade = 20;
+// console.log("Idade : " + idade);
+// idade = 21;
 
 //2.Hoisting
- 
+//Diferença entre var e let
+var nome = "João";//declara como var são tiradas de ond estao em colocam no topo do programa uma embaixo da outra
+let sobrenome = "Silva";
+//usa let que não tem erro pra n dar problena
+if(true){
+    var nome = "Silva"
+}
+
+console.log(nome)
+
 //3.Tipos de dados
  
 //4.Operadores
