f30a5308539c9e1a280c648ba3b1409fd8facd7d
arthuramosantos <arthuramossan@gmail.com>
2024-04-30 09:43:21 -0300

    aulaaa

--- js/script.js
+++ js/script.js
@@ -18,22 +18,49 @@ btnElemento.addEventListener("click",function () {
 
 //2.Hoisting
 //Diferença entre var e let
-var nome = "João";//declara como var são tiradas de ond estao em colocam no topo do programa uma embaixo da outra
-let sobrenome = "Silva";
-//usa let que não tem erro pra n dar problena
-if(true){
-    var nome = "Silva"
-}
+// var nome = "João";
 
-console.log(nome)
+// if(true){
+//     let nome = "Silva";
+// }
+// console.log(nome);
 
 //3.Tipos de dados
- 
+let tiposDeDados = [
+    "Number",
+    "String",
+    "Boolean",
+    "Null",
+    "Undefined",
+    "Object",
+    "Array",
+    "Function"
+] 
+//Imprimindo o array com console.table.
+console.table(tiposDeDados);
+
+
 //4.Operadores
  
 //5.Estruturas condicionais
 
-//6.Estruturas de repetição
+//6.Estruturas de repetição//vai percorer toda estrutura pra gente
+// //Imprimindo Array com forEach com função anônima
+// tiposDeDados.forEach(function(tipo){
+//     console.log(tipo);
+// });
+//iterar sobre um array de tipos de dados que serão inseridos em uma lista ul no html
+//recuperando a lista ul atraves do id
+let listaTiposDeDados = document.getElementById("tipo-dados");
+
+tiposDeDados.forEach((tipo)=>{
+    //Criando um elemento <li> para inserir na <ul>
+    let itemLista = document.createElement("li");
+    //Adicionando o texto do tipo de dado
+     itemLista.innerText = tipo;
+     //Inserindo o elemento <li> na <ul>
+     listaTiposDeDados.appendChild(itemLista);
+});
 
 //7.Funções
 
