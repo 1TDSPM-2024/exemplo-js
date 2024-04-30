510721f6f903c70a21661bbf196525cc51073d99
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-04-30 09:43:21 -0300

    Arrays, forEach append.

--- js/script.js
+++ js/script.js
@@ -29,12 +29,47 @@
 // console.log(nome);
 
 //3.Tipos de dados
- 
+    
+let tiposDeDados = [
+    "Number",
+    "String",
+    "Boolean",
+    "Null",
+    "Undefined",
+    "Object",
+    "Array",
+    "Function"
+]
+
+//Imprimindo o array com console.table.
+console.table(tiposDeDados);
+
+
 //4.Operadores
  
 //5.Estruturas condicionais
 
 //6.Estruturas de repetição
+// //Imprimindo Array com forEach com função anônima
+// tiposDeDados.forEach(function(tipo){
+//     console.log(tipo);
+// });
+// //Imprimindo Array com forEach com arrowFunction
+// tiposDeDados.forEach((tipo)=>{
+//     console.log(tipo);
+// });
+//Itera sobre um array de tipos de dados que serão inseridos em uma lista ul no html de id tipo-dados
+//Recuperando a lista ul através do id
+let listaTiposDeDados = document.getElementById("tipo-dados");
+
+tiposDeDados.forEach((tipo)=>{
+    //Criando um elemento <li> para inserir na <ul>
+    let itemLista = document.createElement("li");
+    //Adicionando o texto do tipo de dado
+     itemLista.innerText = tipo;
+     //Inserindo o elemento <li> na <ul>
+     listaTiposDeDados.appendChild(itemLista);
+});
 
 //7.Funções
 
