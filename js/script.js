f0016d5ca05e662bca21c15de96005b49cb4684d
gabrielfalanga <gabrielmartinsfalanga@gmail.com>
2024-04-30 09:26:08 -0300

    var / let / const / hoisting

--- js/script.js
+++ js/script.js
@@ -1,32 +1,51 @@
 
 
 //JavaScript básico tópicos
-console.log("Olá mundo!");
+// console.log("Olá mundo!");
 
-//Capturar o botão pelo ID e atrelar a ele um evento de click
-const btnElemento = document.getElementById("botao");
-btnElemento.addEventListener("click", function(){
-    console.log("Botão testado.")
-});
+//---Capturar o botão pelo ID e atrelar a ele um evento de click
+// const btnElemento = document.getElementById("botao");
+// btnElemento.addEventListener("click", function(){
+//     console.log("Botão testado.")
+// });
 
-//1.Declaração de variáveis
 
-//2.Hoisting
+// //1.Declaração de variáveis
+// var nome = "João";
+// let sobrenome = "Silva";
+// console.log("Nome completo: " + nome + sobrenome);
+
+// //2.Declaração de constantes
+
+// const idade = 20;
+// console.log("Idade: " + idade);
+
+
+//3.Hoisting
+// diferença entre var e let
+// var nome = "João";
+
+// if(true){
+//     let nome = "Silva"
+// }
+
+// console.log(nome);
+
+
+//4.Tipos de dados
  
-//3.Tipos de dados
+//5.Operadores
  
-//4.Operadores
- 
-//5.Estruturas condicionais
+//6.Estruturas condicionais
 
-//6.Estruturas de repetição
+//7.Estruturas de repetição
 
-//7.Funções
+//8.Funções
 
-//8.Arrays
+//9.Arrays
 
-//9.Objetos
+//10.Objetos
 
-//10.Funções de array
+//11.Funções de array
 
 
