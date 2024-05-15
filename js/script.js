1a791085030c38b8aade28fd5b10448a6f8d2d64
gabrielfalanga <gabrielmartinsfalanga@gmail.com>
2024-05-15 09:59:20 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,113 +1,24 @@
+let listaUsuarios = [
+    {nome: 'João', genero: 'm', email: 'joao@email.com', senha: '12345'},
+    {nome: 'Maria', genero: 'f', email: 'maria@email.com', senha: '12345'},
+    {nome: 'Gabriel', genero: 'm', email: 'gabriel@email.com', senha: '12345'},
+    {nome: 'Debora', genero: 'f', email: 'debora@email.com', senha: '12345'}
+]
 
 
-//JavaScript básico tópicos
-// console.log("Olá mundo!");
+function validacao(inputEmail, inputSenha) {
 
-//---Capturar o botão pelo ID e atrelar a ele um evento de click
-// const btnElemento = document.getElementById("botao");
-// btnElemento.addEventListener("click", function(){
-//     console.log("Botão testado.")
-// });
+    for (let x = 0; x < listaUsuarios.length; x++) {
 
-
-// --------- 1.Declaração de variáveis
-// var nome = "João";
-// let sobrenome = "Silva";
-// console.log("Nome completo: " + nome + sobrenome);
-
-// --------- 2.Declaração de constantes
-
-// const idade = 20;
-// console.log("Idade: " + idade);
-
-
-// --------- 3.Hoisting
-// diferença entre var e let
-// var nome = "João";
-
-// if(true){
-//     let nome = "Silva"
-// }
-
-// console.log(nome);
-
-
-// --------- 4.Tipos de dados
-
-// let tiposDeDados = [
-//     "Number",
-//     "String",
-//     "Boolean",
-//     "Null",
-//     "Undefined",
-//     "Object",
-//     "Array",
-//     "Function"
-// ]
-
-// console.table(tiposDeDados)
-
-// --------- 5.Operadores
- 
-// --------- 6.Estruturas condicionais
-
-// --------- 7.Estruturas de repetição
-
-// let listaTiposDados = document.getElementById("tipo-dados");
-
-// tiposDeDados.forEach((tipo)=>{
-//     // cria <li> como itemLista
-//     let itemLista = document.createElement("li");
-//     // adiciona o texto dentro da <li>
-//     itemLista.innerText = tipo;
-//     // adiciona o <li> na <ul>
-//     listaTiposDados.appendChild(itemLista)
-// });
-
-
-// --------- 8.Funções
-
-// function soma(a,b){
-//     if((a+b) >= 10){
-//         console.log(`A soma de ${a} e ${b} é >= 10.`);
-//         return true;
-//     } else {
-//         console.log(`A soma de ${a} e ${b} é < 10.`);
-//         return false;
-//     }
-// }
-
-// transferindo para ARROW FUNCTION
-
-// const soma = (a,b)=>{
-//     if((a+b) >= 10){
-//         console.log(`A soma de ${a} e ${b} é >= 10.`);
-//         return true;
-//     } else {
-//         console.log(`A soma de ${a} e ${b} é < 10.`);
-//         return false;
-//     }
-// }
-
-
-// RECUPERAR CAMPOS DO FORMULARIO -> querySelector
-
-function validacao(inputEmail, inputSenha){
-
-    let usuario = {
-        email: inputEmail.value,
-        senha: inputSenha.value
+        if((inputEmail.value == u.email) && (inputSenha.value == u.senha)){
+            console.log("Usuário", u.nome, "encontrado.");
+            return false;
+        } else {
+            console.log("Usuário não encontrado.")
+            return true;
+        }
     };
 
-    usuario = {...usuario, nomeCompleto:"Alê Gênio"};
-
-    const{email,senha,nomeCompleto}=usuario;
-
-    if((inputEmail.value == "") && (inputSenha.value == "")){
-        alert("É necessário preencher os campos obrigatórios.");
-        return false;
-    }
-    return true;
 }
 
 // --------- 9.Arrays
