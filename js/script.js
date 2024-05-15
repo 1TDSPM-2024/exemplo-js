1e28fadc8249a8499a52dc12f3691178091b711a
Erick0105 <erickspot01@gmail.com>
2024-05-15 09:56:18 -0300

    exercicio com validação com objetos e arrays

--- js/script.js
+++ js/script.js
@@ -102,28 +102,58 @@
 // const inputEmail = document.querySelector("#idEmail");
 // const inputSenha = document.querySelector("#idSenha");
 
+let listaUsuarios = [
+  {nome:"João", genero:"m", email:"jo@email.com", senha:"123456"},
+  {nome:"Maria", genero:"f", email:"ma@email.com", senha:"123456"},
+  {nome:"Roberta", genero:"f", email:"ro@email.com", senha:"123456"},
+  {nome:"Antonio", genero:"m", email:"an@email.com", senha:"123456"},
+  {nome:"Carlos", genero:"m", email:"ca@email.com", senha:"123456"},
+  
+]
+
+
 function validacao(inputEmail,inputSenha) {
 
     //Recupere os dados dos campos e adicione em um objeto de nome usuário e desestruture o objeto
     //pegando as propriedades e realizando uma validação para ver se as propriedades possuem valores de preenchimento.
     //Após a validação, adicione uma nova propriedade ao objeto de nome nomeCompleto com o valor de sua escolha.
 
+    //if((inputEmail.value == "") && (inputSenha.value == "")){
+    //    alert("O preenchimento dos campos são obrigatórios.");
+    //    return false;
+    //} 
+    //  let usuario = {
+    //    emailUsuario : inputEmail.value,
+    //    senhaUsuario : inputSenha.value 
+    //  }
     let usuario = {
-        emailUsuario : inputEmail.value,
-        senhaUsuario : inputSenha.value
-    };
-
-    if((inputEmail.value == "") && (inputSenha.value == "")){
-        alert("O preenchimento dos campos são obrigatórios.");
-        return false;
+      emailUsuario : inputEmail.value,
+      senhaUsuario : inputSenha.value 
     }
-    return true;
+
+  for (let x = 0; x < listaUsuarios.length; x++){
+    if((inputEmail.value == listaUsuarios[x].email) && (inpuSenha.value == listaUsuarios[x].senha)){
+      console.log("Usuário encontrado")
+      return true;
+    }else{
+      console.log("Usuario não encontrado")
+      return false;
+    }
+  }
 }
+;
+//document.getElementById('idCpf').addEventListener('input', function(e) {
+//  var value = e.target.value;
+//  var cpfPattern = value.replace(/\D/g, '')
+//                        .replace(/(\d{3})(\d)/, '$1.$2')
+//                        .replace(/(\d{3})(\d)/, '$1.$2')
+//                        .replace(/(\d{3})(\d)/, '$1-$2')
+//                        .replace(/(-\d{2})\d+?$/, '$1');
+//  e.target.value = cpfPattern;
+//});
 
 
 //8.Arrays
-
-
 
 // //9.Objetos
 // let usuario = {
@@ -150,5 +180,3 @@ function validacao(inputEmail,inputSenha) {
 // console.log(`Região de Residência: ${regiao}`);
 
 //10.Funções de array
-
-
